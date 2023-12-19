3e1dc0ac54b626dad96bb699ead4301e08feee1b
Tonyopano <107053802+Tonyopano@users.noreply.github.com>
2023-12-19 15:02:45 +0300

    creating dammy components to be imported in App component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
     </div>
   )
 }
---
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PlaceDetails = () => {
+  return (
+    <h1>Header</h1>
+  );
+}
+
+export default PlaceDetails;
