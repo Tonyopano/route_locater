ce39a525c7f1a0a39aba7ef865f7d21a816559ed
Tonyopano <107053802+Tonyopano@users.noreply.github.com>
2023-12-22 01:24:22 +0300

    Adjusting List section with grading based on search feature

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from "@material-ui/core";
+
+import useStyles from './styles';
 
 const List = () => {
+  const classes = useStyles();
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
+
   return (
-    <h1>List</h1>
+    <div className={classes.container}>
+      <Typography variant="h4">Restaurants, Hotels & Attractions around you</Typography>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Type</InputLabel>
+        <select value={type} onChange={(e) => setType(e.target.value)}> 
+          <MenuItem value="restaurants">restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel>Rating</InputLabel>
+        <select value={rating} onChange={(e) => setRating(e.target.value)}> 
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
+        </select>
+      </FormControl>
+    </div>
   );
 }
 
