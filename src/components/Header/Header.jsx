fd6f7689e5d8cf0c675bd0de36fbebf95df0b84b
Tonyopano <107053802+Tonyopano@users.noreply.github.com>
2023-12-21 14:52:36 +0300

    Header style fix's

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import { Autocomplete } from "@react-google-maps/api";
+import { AppBar, ToolBar, Typography, InputBase, Box } from "@material-ui/core";
+import { Search } from "@material-ui/icons/Search";
 
 const Header = () => {
   return (
-    <h1>Header</h1>
+    <AppBar position="static">
+      <ToolBar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Route locater
+        </Typography>
+        <Box display="flex">
+            <Typography variant="h6" className={classes.title}>
+              Explore new routes
+            </Typography>
+            <Autocomplete>
+              <div className={classes.search}>
+                <div className={classes.searchIcon}>
+                  <searchIcon />
+                </div>
+                <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+              </div>
+            </Autocomplete>
+        </Box>
+      </ToolBar>
+    </AppBar>
   );
 }
 
