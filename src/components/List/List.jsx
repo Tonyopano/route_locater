3f19b22dc08788c9eb71bf8625392b42f79dbee4
Tonyopano <107053802+Tonyopano@users.noreply.github.com>
2023-12-22 23:32:35 +0300

    Embending places on list

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -8,6 +8,11 @@ const List = () => {
   const [type, setType] = useState('restaurants');
   const [rating, setRating] = useState('');
 
+  const places = [
+    {name: 'Cool Place',},
+    {name: 'Best Beer'},
+  ];
+
   return (
     <div className={classes.container}>
       <Typography variant="h4">Restaurants, Hotels & Attractions around you</Typography>
