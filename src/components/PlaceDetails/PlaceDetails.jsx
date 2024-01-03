b28dbe017f5c34c5dd142a7b5693fb154ce07b64
Tonyopano <107053802+Tonyopano@users.noreply.github.com>
2024-01-03 23:12:58 +0300

    card elevation

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React from 'react';
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import Rating from '@material-ui/lab/Rating';
 
-const PlaceDetails = ({ place }) => {
+import useStyles from './styles.js';
+
+const PlaceDetails = ({ place, selected, refProp }) => {
+  if (selected) refProp?.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  const classes = useStyles();
+
   return (
-    <h1>{place.name}</h1>
-  );
-}
-
-export default PlaceDetails;
+    <Card elevation={6}>
+      <CardMedia
+        style={{ height: 350 }}
+        image={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+        title={place.name}
+      />
