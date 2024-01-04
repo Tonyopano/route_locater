af5a2271851d89599caa354a5771ec439b3f6ebd
Tonyopano <107053802+Tonyopano@users.noreply.github.com>
2024-01-04 06:44:07 +0300

    weather data implementation

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,30 +1,57 @@
-import React from "react";
+import React from 'react';
 import GoogleMapReact from 'google-map-react';
-import { Paper, Typography , useMediaQuery } from '@material-ui/core';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import Rating from '@material-ui/lab';
+import Rating from '@material-ui/lab/Rating';
 
-import useStyles from './styles';
+import mapStyles from '../../mapStyles';
+import useStyles from './styles.js';
 
-const Map = () => {
+const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherData }) => {
+  const matches = useMediaQuery('(min-width:600px)');
   const classes = useStyles();
-  const isMobile = useMediaQuery('(min-width:600px)');
-
-  const coordinates = { lat: 0, lng: 0 };
 
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLkeys={{ key: 'AIzaSyBbBQe9XiFa-qi7RuBUkDlEJP6bgFt1SIQ' }}
-        defaultCenter={coordinates}
-        center={coordinates}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        defaultCenter={coords}
+        center={coords}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
-        options={''}
-        onChange={''}
-        onClildClick={''}
+        options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
+        onChange={(e) => {
+          setCoords({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        onChildClick={(child) => setChildClicked(child)}
       >
-
+        {places.length && places.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {!matches
+              ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
+                  <img
+                    className={classes.pointer}
+                    src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                  />
+                  <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                </Paper>
+              )}
+          </div>
+        ))}
+        {weatherData?.list?.length && weatherData.list.map((data, i) => (
+          <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+            <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" />
+          </div>
+        ))}
       </GoogleMapReact>
     </div>
   );
